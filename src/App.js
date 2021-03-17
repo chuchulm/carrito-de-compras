15851fe2d29d36c5f4b810afeae5843e633a5fb8
Jesus Luciani <jesusluciani833@hotmail.com>
2021-03-17 16:27:05 -0400

    segunda copia

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 
+import data from "./data.json"
+import { useState } from "react";
+import { Products } from "./components/Products";
 
 
 function App() {
+  
+  const [dataBase, setDataBase] = useState({
+    products: data.products,
+    size: '',
+    sort: '',
+
+  })
+ 
+
+  const { products, size, sort } = dataBase
+
+
   return (
     <div className="grid-container">
-    <header>
-      <a href="/">React Shopping cart</a>
-    </header>
-    <main>
-      product list
-    </main>
-    <footer>
-      all right is reserved.
-    </footer>
+
+      <header>
+        <a href="/">React Shopping cart</a>
+      </header>
+
+      <main>
+          <div className="content">
+    
+            <div className="main">
+               <Products products={ products }/>
+
+            </div>
+
+            <div className="sidebar">
+              Cart Items
+
+            </div>
+      
+          </div>
+      </main>
+
+      <footer>
+        all right is reserved.
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
+// 1:04.38
